c6d394f461bd9ff3dd756ec9648e92af7f867fd3
masaks <oicwam.orz1228@gmail.com>
2020-04-04 17:54:26 +0900

    command 実行時にログ出力

--- src/domain/CommandExec.ts
+++ src/domain/CommandExec.ts
@@ -4,22 +4,26 @@ export class CommandExec {
     private _targetRepository = '';
 
     public cp(savePath: string) {
+        console.log('作成されたファイルをブログアプリにコピー');
         childProcess.execSync(`cp ${savePath} ~/angular-scully-blog/blog/`);
     }
 
     public gitAdd() {
+        console.log('ブログアプリのリポジトリでgit add');
         childProcess.execSync(
             `git --work-tree=${this._targetRepository} --git-dir=${this._targetRepository}/.git add ${this._targetRepository}/blog/.`
         );
     }
 
     public gitCommit() {
+        console.log('ブログアプリのリポジトリでgit commit');
         childProcess.execSync(
             `git --work-tree=${this._targetRepository}/ --git-dir=${this._targetRepository}/.git commit -b "Create new article with Type and Push from API Server"`
         );
     }
 
     public gitPush() {
+        console.log('ブログアプリのリポジトリでgit push');
         childProcess.execSync(
             `git --work-tree=${this._targetRepository}/ --git-dir=${this._targetRepository}/.git push`
         );
